dbb51604e559f3cd9fc3d9d53c3c661ecc0b1d33
Template Expander <noreply@example.com>
2024-11-09 14:19:13 +0000

    [workflow] expand install.toml template fe03276663356c0bfcfb05b53d5cf8273206755b

---
+++ dist/compile.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+set -eu
+
+ARGS=(
+    "-std=gnu++23"
+
+    -DONLINE_JUDGE
+    -DATCODER
+
+    -O2
+
+    "-mtune=native"
+    "-march=native"
+
+    -Wall
+    -Wextra
+
+    "-fconstexpr-depth=2147483647"
+    "-fconstexpr-loop-limit=2147483647"
+    "-fconstexpr-ops-limit=2147483647"
+
+    -I/opt/ac-library/
+    -I/opt/boost/gcc/include/ -L/opt/boost/gcc/lib/
+    -I/usr/include/eigen3/
+    -lgmpxx -lgmp
+)
+
+g++-14 ./Main.cpp -o a.out "${ARGS[@]}"
