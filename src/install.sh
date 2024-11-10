3ffc05c447f06703f29eb327cb103b0f8b35da02
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 21:31:43 +0900

    modify installers: fix paths

--- src/install.sh
+++ src/install.sh
@@ -9,8 +9,6 @@ sudo apt-get install -y build-essential
 sudo apt-get install pigz
 sudo apt-get install pbzip2
 
-cd /tmp
-
 ./sub-installers/AC-Library.sh
 ./sub-installers/Boost.sh
 ./sub-installers/Eigen.sh
