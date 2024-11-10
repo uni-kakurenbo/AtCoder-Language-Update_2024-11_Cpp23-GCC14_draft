31fc298b176a0897b68d685666cad53ac0f81b68
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-11 00:53:26 +0900

    modify workflows: remove comments

--- src/sub-installers/libtorch.sh
+++ src/sub-installers/libtorch.sh
@@ -11,5 +11,3 @@ sudo mkdir -p /opt/libtorch/lib/libtorch/
 
 sudo cp -Trf ./libtorch/include/ /opt/libtorch/include/
 sudo cp -Trf ./libtorch/lib/ /opt/libtorch/lib/
-
-# g++ example-app.cpp -I${install_root}/include -I${install_root}/include/torch/csrc/api/include -D_GLIBCXX_USE_CXX11_ABI=0 -std=gnu++14 -L${install_root}/lib -Wl,-R${install_root}/lib -ltorch -ltorch_cpu -lc10 -o example-app
