e5b44b1b9a9887fd802a67f305361132e39c2ccf
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 11:23:46 +0900

    modify installer: parallel boost build

--- src/sub-installers/Boost.sh
+++ src/sub-installers/Boost.sh
@@ -3,13 +3,13 @@ set -eu
 
 mkdir -p ./boost/
 
-sudo wget "https://archives.boost.io/release/${VERSION}/source/boost_${VERSION//./_}.tar.bz2" -O boost.tar.bz2
-sudo tar -jvxf boost.tar.bz2 -C ./boost/ --strip-components 1
+sudo wget -q "https://archives.boost.io/release/${VERSION}/source/boost_${VERSION//./_}.tar.bz2" -O boost.tar.bz2
+sudo tar -I pbzip2 -xf boost.tar.bz2 -C ./boost/ --strip-components 1
 
 cd ./boost/
 
 sudo ./bootstrap.sh --with-toolset=gcc --without-libraries=mpi,graph_parallel
 
-BUILD_ARGS=("-j3" "toolset=gcc" "threading=single" "variant=release" "link=static" "runtime-link=static" "cxxflags=\"-std=gnu++23\"")
+BUILD_ARGS=(-d0 "-j$(nproc)" "toolset=gcc" "threading=single" "variant=release" "link=static" "runtime-link=static" "cxxflags=\"-std=gnu++23\"")
 sudo ./b2 "${BUILD_ARGS[@]}" stage
-sudo ./b2 "${BUILD_ARGS[@]}" --prefix=/opt/boost/gcc install
+sudo ./b2 "${BUILD_ARGS[@]}" --prefix=/opt/boost/gcc/ install
