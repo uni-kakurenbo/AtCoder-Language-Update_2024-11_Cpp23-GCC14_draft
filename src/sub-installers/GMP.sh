12b0648fa56670b0ddec0992f9c08044c412f550
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 09:22:52 +0900

    modify installers: use apt for GMP

--- src/sub-installers/GMP.sh
+++ src/sub-installers/GMP.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 set -eu
 
-mkdir -p ./gmp/
-
-sudo wget "https://ftp.gnu.org/gnu/gmp/gmp-${VERSION}.tar.xz" -O gmp.tar.xz
-sudo tar -Jvxf gmp.tar.xz -C ./gmp/ --strip-components 1
-
-cd ./gmp/
-
-sudo ./configure --enable-cxx --prefix=/opt
-sudo make all
-sudo make install
+sudo apt-get install -y "libgmp3-dev=${VERSION}"
