fd94753fe6d4fac28b12e023ace5d9b6152395f2
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 22:06:34 +0900

    refactor installers: fix paths

--- src/sub-installers/unordered_dense.sh
+++ src/sub-installers/unordered_dense.sh
@@ -3,8 +3,8 @@ set -eu
 
 mkdir -p ./unordered_dense/
 
-sudo wget "https://github.com/martinus/unordered_dense/archive/refs/tags/v${VERSION}.tar.gz" -O unordered_dense.tar.gz
-sudo tar -I pigz -vxf unordered_dense.tar.gz -C ./unordered_dense/ --strip-components 1
+sudo wget "https://github.com/martinus/unordered_dense/archive/refs/tags/v${VERSION}.tar.gz" -O ./unordered_dense.tar.gz
+sudo tar -I pigz -vxf ./unordered_dense.tar.gz -C ./unordered_dense/ --strip-components 1
 
 cd ./unordered_dense/
 
