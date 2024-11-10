238ebf7a9f8f289614721710d35eda3311546b85
Template Expander <noreply@example.com>
2024-11-10 13:37:12 +0000

    [workflow] expand install.toml template eb0f4adc6c260c90d897f682fd23e58d87d9cdd9

--- dist/sub-installers/AC-Library.sh
+++ dist/sub-installers/AC-Library.sh
@@ -6,7 +6,7 @@ VERSION="1.5.1"
 
 set -eu
 
-cd /tmp
+cd /tmp/
 
 sudo wget -q "https://github.com/atcoder/ac-library/releases/download/v${VERSION}/ac-library.zip" -O ./ac-library.zip
 sudo unzip -q ./ac-library.zip -d /opt/ac-library/
