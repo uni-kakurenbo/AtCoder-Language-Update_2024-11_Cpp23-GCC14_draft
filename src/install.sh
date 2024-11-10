39bf046083605a10eaca6fe49d5c65dfcda556f8
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 11:23:46 +0900

    modify installer: parallel boost build

--- src/install.sh
+++ src/install.sh
@@ -6,6 +6,9 @@ sudo apt-get install -y "g++-14=${VERSION}"
 
 ### Libraries
 sudo apt-get install -y build-essential
+sudo apt-get install pigz
+sudo apt-get install pbzip2
+
 cd /tmp
 
 ./sub-installers/AC-Library.sh
