4cd07ca653cc14da29996cc87865511d7c8911e8
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 22:53:38 +0900

    modify installers: add -o to unzip

--- src/sub-installers/AC-Library.sh
+++ src/sub-installers/AC-Library.sh
@@ -4,4 +4,4 @@ set -eu
 cd /tmp/
 
 sudo wget -q "https://github.com/atcoder/ac-library/releases/download/v${VERSION}/ac-library.zip" -O ./ac-library.zip
-sudo unzip -q ./ac-library.zip -d /opt/ac-library/
+sudo unzip -oq ./ac-library.zip -d /opt/ac-library/
