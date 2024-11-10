e6fbc7492dd926962ab8e8c10a8654621d22445a
uni_kakurenbo <kohma1116@ybb.ne.jp>
2024-11-10 09:23:20 +0900

    modify workflow: pretty version field

--- .github/workflows/internal/replacers/config.sh
+++ .github/workflows/internal/replacers/config.sh
@@ -12,4 +12,7 @@ TARGET="${TARGET//"\${{ref:installer}}"/"\n${INSTALLER}\n"}"
 
 cat ./assets/warning.txt >./dist/install.toml
 echo >>./dist/install.toml
+
 echo -e "${TARGET//"\${{ref:builder}}"/"\n${BUILDER}\n"}" >>./dist/install.toml
+
+sed -i -r "s/version\s*=\s*[\'\"][^\.]*(([0-9]+\.)+[0-9]+).*/version = '\1'/" ./dist/install.toml
