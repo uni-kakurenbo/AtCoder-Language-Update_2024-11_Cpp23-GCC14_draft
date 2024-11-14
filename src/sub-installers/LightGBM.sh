0c347f96c2aa85c6c74aa1119cefd004b206da7e
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-11-14 23:18:03 +0000

    modify installer: LightGBM

--- src/sub-installers/LightGBM.sh
+++ src/sub-installers/LightGBM.sh
@@ -3,9 +3,15 @@ set -eu
 
 cd /tmp/
 
-sudo wget -q https://github.com/microsoft/LightGBM/releases/download/v4.5.0/lightgbm-4.5.0.tar.gz -O ./light-gbm.tar.gz
+mkdir -p ./light-gbm/
+
+sudo wget -q "https://github.com/microsoft/LightGBM/releases/download/v${VERSION}/lightgbm-${VERSION}.tar.gz" -O ./light-gbm.tar.gz
+sudo tar -I pigz -xf ./light-gbm.tar.gz -C ./light-gbm/ --strip-components 1
+
+cd ./light-gbm/
+sudo rm -rf ./lightgbm/
 
 mkdir -p ./build/ && cd ./build/
 
-cmake -DCMAKE_CXX_FLAGS:STRING="${COMPILE_OPTIONS[*]}" -S ./
-cmake --build build --parallel "${PARALLEL}"
+sudo cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/light-gbm/ -DCMAKE_CXX_FLAGS:STRING="${COMPILE_OPTIONS[*]}" ../
+sudo cmake --build ./ --target install --parallel "${PARALLEL}"
