2ccb79969486d2e67c60999684d6387af939899e
みどりむし <64454054+uni-kakurenbo@users.noreply.github.com>
2024-11-15 05:43:14 +0000

    fixup! modify installer: remove already installed library

--- src/sub-installers/LightGBM.sh
+++ src/sub-installers/LightGBM.sh
@@ -9,10 +9,11 @@ sudo wget -q "https://github.com/microsoft/LightGBM/releases/download/v${VERSION
 sudo tar -I pigz -xf ./light-gbm.tar.gz -C ./light-gbm/ --strip-components 1
 
 cd ./light-gbm/
+
 sudo rm -rf ./lightgbm/
 sudo rm -rf ./external_libs/eigen/
 
 mkdir -p ./build/ && cd ./build/
 
-sudo cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/light-gbm/ -DCMAKE_CXX_FLAGS:STRING="${INTERNAL_BUILD_FLAGS[*]}" ../
+sudo cmake -DCMAKE_INSTALL_PREFIX:PATH=/opt/light-gbm/ -DCMAKE_CXX_FLAGS:STRING="${INTERNAL_BUILD_FLAGS[*]} -I/usr/include/eigen3/" ../
 sudo cmake --build ./ --target install --parallel "${PARALLEL}"
